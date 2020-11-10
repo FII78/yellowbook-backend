62588d5cf625b1c333b7d2d881c60ed153473b3d
Fitsum Sileshi <fitsesile@gmail.com>
2020-11-11 01:18:51 +0300

    meta controller added

--- Controllers/MetaController.cs
+++ Controllers/MetaController.cs
@@ -1,13 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FindIt.API.Controllers
 {
+    [Route("api/meta")]
+    [ApiController]
     public class MetaController : Controller
     {
-       
+        [HttpGet]
+        [Route("aboutUs")]
+        [AllowAnonymous]
+        public ContentResult AboutUs()
+        {
+            var variable = "https://something.com";
+            var result = new ContentResult
+            {
+                ContentType = "text/html",
+                StatusCode = (int)HttpStatusCode.OK,
+                Content = "<html>" +
+                "<body>" +
+                "<div><p><b>Bla Bla</b></p></div>" +
+                $"<p>click <a href={variable}> Doc </a></p>" +
+                "/body" +
+                "</html>"
+            };
+            return result;
+        }
     }
 }
